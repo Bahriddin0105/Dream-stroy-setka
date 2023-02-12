76984bcd3f3ddaf47dd65ddfd81d367af25d6042
zafarDilmurodov <104644553+zafarDilmurodov@users.noreply.github.com>
2023-02-13 03:23:52 +0500

    .

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from "react";
 import Header from "./jss/Header";
 import Onesheet from "./jss/Onesheet";
+import Twosheet from "./jss/Twosheet";
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <Header />
-        <Onesheet />
+        {/* <Header /> */}
+        {/* <Onesheet /> */}
+        <Twosheet />
       </div>
     );
   }
--- src/jss/Twosheet.js
+++ src/jss/Twosheet.js
@@ -1,11 +1,44 @@
 import React, { Component } from "react";
 import "../css/twosheet.css";
+import one from "../image/three.png";
+import two from "../image/four.png";
+import three from "../image/five.png";
+import four from "../image/five1.png";
 
 export default class Twosheet extends Component {
   render() {
     return (
       <div className="twosheet">
-        <div></div>
+        <div className="infobox">
+          <div className="twoborder">
+            <h3 className="twoh3">Хотите</h3>
+            <h2 className="twoh2">заказать у нас?</h2>
+            <p className="twop">
+              Оставьте заявку и мы свяжимся с вами в течении 20 минут
+            </p>
+            <span className="inputname">
+              <label className="namelabel">Введите имя</label>
+              <input className="inname" type="text" placeholder="Имя" />
+            </span>
+            <span className="inputnum">
+              <label className="numlabel">Введите номер</label>
+              <input className="innum" type="number" placeholder="+998" />
+            </span>
+            <span className="twosheetbtn">
+              <p>Оставьте заявку</p>
+            </span>
+            <p className="twosheetend">
+              Нажимая “Оставить заявку” вы даете согласие на обработку ваших
+              данных
+            </p>
+          </div>
+        </div>
+        <div className="imgbox">
+          <img className="oneimg" src={one} alt="" />
+          <img className="twoimg" src={three} alt="" />
+          <img className="threeimg" src={four} alt="" />
+          <img className="fourimg" src={two} alt="" />
+        </div>
       </div>
     );
   }
